520d26853ef4af1cfca9741fe03e1e2c7917b9f3
Roey Yadgar <roaiyadgar@gmail.com>
2024-01-25 12:59:20 +0200

    added functions to commit: covar_cost_func , covar_gradient_native , cryo_project_arr , transpoeTensor

---
+++ covar_cost_func.m
@@ -0,0 +1,22 @@
+function cost_val = covar_cost_func(y,Pu)
+    %cost_func = ||y||^4 -2*(<y,\Sum Pu>)^2 + ||\Sum Pu||^4 (fix cuz that
+    %shit aint true)
+    L = size(Pu,1);
+    r = size(Pu,3);
+    n = size(Pu,4);
+    y = reshape(y,L,L,1,n);
+
+    norm_y_term = squeeze(sum(y.*y,[1,2])).^2;
+
+    y_Pu_inner_prod = (sum(y.*Pu,[1,2]));
+    y_Pu_term = squeeze(sum(y_Pu_inner_prod.^2,3));
+    %y_Pu_term = squeeze(sum((sum((sum(y.*Pu,[1,2])).*Pu,3)).*y,[1,2,3]));
+    
+    
+    Pu = reshape(Pu,L^2,r,n);
+    Pu_gram = pagemtimes(Pu,'transpose',Pu,'none');
+    Pu_term = squeeze(sum((Pu_gram).^2,[1,2]));
+
+    cost_val = mean(norm_y_term - 2*y_Pu_term + Pu_term)/(L^4); %validate L^4 normalization
+
+end
