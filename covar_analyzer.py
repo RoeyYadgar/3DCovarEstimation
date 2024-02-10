updates utils functions, corrected gradient normalization,updated covar_analyzer plotting, updated covar_estimate_test with rank-2 testing

covar_analyzer.py
@@ -73,11 +73,12 @@ class CovarAnalyzer():
                 label = ','.join(f'{column}_{self.dataframe.iloc[i][column]}' for column in self.dataframe.columns if column != 'filename')
                 plt.plot(covar.epoch_ind_log,metric(covar),label=label)
         
-        plt.show()
+        #plt.show()
         plt.legend()
         
     def plotCosineSim(self):
-        cosine_sim_metric = lambda covar: np.abs(covar.cosine_sim_log)
+        #cosine_sim_metric = lambda covar: np.abs(covar.cosine_sim_log)
+        cosine_sim_metric = lambda covar : np.abs([np.mean(np.sqrt(np.sum(covar.cosine_sim_log[i] ** 2,axis = 0))) for i in range(len(covar.cosine_sim_log))])
         self.plotMetric(cosine_sim_metric)
         
     def plotCostval(self):

covar_estimate_test.py
@@ -2,7 +2,7 @@ from utils import *
 from covar_estimation import *
 from covar_sgd import Covar
 
-from aspire.volume import Volume
+from aspire.volume import Volume,LegacyVolume
 from aspire.utils import Rotation
 from aspire.source import Simulation
 import scipy
@@ -10,59 +10,60 @@ import torch
 import pickle
 import os
 import pandas as pd
+aspire.config['logging']['console_level'] = 'WARNING'
 
 L = 15
 n = 2048
-voxels = Volume.from_vec(np.concatenate((generateBallVoxel([-0.6,0,0],0.5,15),
-                                         -generateBallVoxel([0.6,0,0],0.5,15))))
-                                         
-#voxels = Volume.from_vec((generateBallVoxel([-0.6,0,0],0.5,15)))
+r = 2
+
+
+voxels = LegacyVolume(
+    L=L,
+    C=r+1,
+    dtype=np.float32,
+).generate()
+
+voxels -= np.mean(voxels,axis=0)
+
+_,voxelsSTD,voxelsSpan = np.linalg.svd((voxels).asnumpy().reshape((r+1,-1)),full_matrices=False)
+voxelsSpan = Volume.from_vec(voxelsSpan[:r,:] * voxelsSTD[:r,np.newaxis]) # r + 1 voxels with 0 mean can be spanned by r voxels
  
 
 mean_voxel = Volume.from_vec(np.zeros((1,L**3),dtype=np.single))
-#voxels.save('tmp.mrc',overwrite=True)
 
 
-sim = Simulation(n = n , vols = replicateVoxelSign(voxels),amplitudes= 1,offsets = 0)
-#projections = sim.images[:]
-#rots = Rotation.from_euler(sim.angles)
-#projections[:5].show()
+sim = Simulation(n = n , vols = voxels,amplitudes= 1,offsets = 0)
+
+c = Covar(L,2,mean_voxel,sim,vectors= None,vectorsGD = voxels[0])
 
 
-#v = voxels.to_vec()[0].reshape((1,L,L,L))
-#v[0,0,0,0]+=10
-'''
-c = Covar(L,1,mean_voxel,sim,vectors= None,vectorsGD = voxels[0])
 
+lr_normalization_factor = np.mean(np.linalg.norm(sim.images[:],axis=(1,2))) ** 2
 
-c.toVol().save('data/test_before.mrc',overwrite= True)
-c.train(batch_size = 1,epoch_num = 1,lr= 100)
-estimated_vol = c.toVol()
-estimated_vol.save('data/test.mrc',overwrite= True)
-'''
+learning_rate = [5,0.5,50]
+momentum = [0.9,0.95,0.8]
+regularization = [1e-6,1e-5,1e-4, 1e-3 ,1e-2]
 
-learning_rate = [10]
-momentum = [0.9]
-regularization = [1e-5,1e-4, 1e-3 ,1e-2]
-
-
+folder_name = 'data/results3'
 covar_dataframe = []
 for lr in learning_rate:
     for mu in momentum:
         for reg in regularization:
-            filepath = f'data/results/covar_est_lr{lr}_momentum{mu}_reg{reg}.bin'
+            filepath = os.path.join(folder_name,f'covar_est_lr{lr}_momentum{mu}_reg{reg}.bin')
             
             if(not os.path.isfile(filepath)):
-                covar_dataframe.append([filepath,lr,mu,reg])
-                c = Covar(L,1,mean_voxel,sim,vectors= None,vectorsGD = voxels)
-                c.train(batch_size = 1,epoch_num = 10,lr= lr,momentum=mu , reg = reg)
-                pickle.dump(c,open(filepath,'wb'))
+                print(f'Running SGD on : {filepath}')
+                open(filepath,'wb') #Create the 'decoy' file so other threads will not run the same training with the same parameters
+                c = Covar(L,r,mean_voxel,sim,vectors= None,vectorsGD = voxelsSpan)
+                try:
+                    c.train(batch_size = 1,epoch_num = 10,lr= lr / lr_normalization_factor,momentum=mu , reg = reg)
+                    pickle.dump(c,open(filepath,'wb'))
+                    appendCSV(pd.DataFrame([[filepath,lr,mu,reg]],columns = ['filename','learning_rate','momentum','reg']),
+                              os.path.join(folder_name,'results.csv'))
+                except Exception:
+                    print('Covar training got nan cost value, skipping training')
+                    os.remove(filepath) #Remove the 'decoy' file
                 
             
-covar_dataframe = pd.DataFrame(covar_dataframe,columns = ['filename','learning_rate','momentum','reg'])
-#covar_dataframe.to_csv('data/results/results.csv',mode = 'a',header = False)
-appendCSV(covar_dataframe, 'data/results/results.csv')
             
 
-c = Covar(L,2,mean_voxel,sim,vectors= None,vectorsGD = voxels)
-c.train(batch_size = 1,epoch_num = 10,lr= 10,momentum=0.9 , reg = 1e-4)

covar_estimation.py
@@ -31,7 +31,7 @@ def covar_cost_gradient(vols_backproject,images_backproject,images_volsforward_p
     
     
     
-    return grad/(L**3)
+    return grad/(L**2)
     
 def covar_cost(vols_forward,images,vols = None,reg = 0):
     #TODO check need for ordering in reshape
@@ -60,7 +60,7 @@ def covar_cost(vols_forward,images,vols = None,reg = 0):
     if(reg != 0):
         vols = vols.asnumpy().reshape((rank,-1))
         vols_prod = np.matmul(vols,vols,axes=[(0,1),(1,0),(0,1)])
-        reg_cost = np.sum(np.power(vols_prod,2),axis=(0,1))/(L**3)
+        reg_cost = np.sum(np.power(vols_prod,2),axis=(0,1))/(L**2)
         cost_val += reg * reg_cost
     else:
         vols_prod = None

utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import os
 import torch
 from numpy import random
 from aspire.utils import coor_trans,Rotation
@@ -42,11 +43,14 @@ def cosineSimilarity(vec1,vec2):
     vec1 = asnumpy(vec1).reshape((vec1.shape[0],-1))
     vec2 = asnumpy(vec2).reshape((vec2.shape[0],-1))
     
-    vec1_norm = np.linalg.norm(vec1,axis=(-1)).reshape((vec1.shape[0],1))
-    vec2_norm = np.linalg.norm(vec2,axis=(-1)).reshape((vec2.shape[0],1))
+    #vec1_norm = np.linalg.norm(vec1,axis=(-1)).reshape((vec1.shape[0],1))
+    #vec2_norm = np.linalg.norm(vec2,axis=(-1)).reshape((vec2.shape[0],1))
     
-    vec1 = vec1/vec1_norm
-    vec2 = vec2/vec2_norm
+    #vec1 = vec1/vec1_norm
+    #vec2 = vec2/vec2_norm
+    
+    vec1 = np.linalg.svd(vec1,full_matrices=False)[2]
+    vec2 = np.linalg.svd(vec2,full_matrices=False)[2]
     
     cosine_sim = np.matmul(vec1,vec2.transpose())
     
@@ -69,7 +73,7 @@ def principalAngles(vec1,vec2):
 
 def asnumpy(data):
     if(type(data) == aspire.volume.volume.Volume or type(data) == aspire.image.image.Image):
-        data = data.asnumpy()
+        return data.asnumpy()
         
     return data
 
@@ -80,7 +84,10 @@ def np2torchDtype(np_dtype):
 
 
 def appendCSV(dataframe,csv_file):
-    current_dataframe = pd.read_csv(csv_file,index_col =0)
-    updated_dataframe = pd.concat([current_dataframe,dataframe],ignore_index = True)
-
-    updated_dataframe.to_csv(csv_file)
+    if(os.path.isfile(csv_file)):
+        current_dataframe = pd.read_csv(csv_file,index_col =0)
+        updated_dataframe = pd.concat([current_dataframe,dataframe],ignore_index = True)
+        updated_dataframe.to_csv(csv_file)
+        
+    else:
+        dataframe.to_csv(csv_file)