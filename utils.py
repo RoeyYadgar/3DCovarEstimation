e4de6b532eaeec357e12b6214c8aec4aa9cc5884
RoeyYadgar <roaiyadgar@gmail.com>
2024-06-09 10:18:04 +0300

    changed cosienSimilarity in utils to use torch tensors

--- utils.py
+++ utils.py
@@ -76,7 +76,7 @@ def nonNormalizedGS(vecs):
     return ortho_vecs
 
 def cosineSimilarity(vec1,vec2):
-    
+    '''
     vec1 = asnumpy(vec1).reshape((vec1.shape[0],-1))
     vec2 = asnumpy(vec2).reshape((vec2.shape[0],-1))
     
@@ -90,8 +90,12 @@ def cosineSimilarity(vec1,vec2):
     vec2 = np.linalg.svd(vec2,full_matrices=False)[2]
     
     cosine_sim = np.matmul(vec1,vec2.transpose())
-    
-    
+    '''
+    vec1 = vec1.reshape((vec1.shape[0],-1))
+    vec2 = vec2.reshape((vec2.shape[0],-1))
+    vec1 = torch.linalg.svd(vec1,full_matrices = False)[2]
+    vec2 = torch.linalg.svd(vec2,full_matrices = False)[2]
+    cosine_sim = torch.matmul(vec1,torch.transpose(vec2,0,1)).cpu().numpy()
     
     return cosine_sim
     
