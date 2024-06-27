72dbd90922d0f3856df78b0b603ee3fb280fdc91
RoeyYadgar <roaiyadgar@gmail.com>
2024-06-27 11:37:53 +0300

    changed IterativeCovar fixed_vectors,fixed_vectors_ampl attribute to not change size by concatentation (but instead intialize it at the correct size and assign each vector in the corresponding index). Added state_dict and load_state_dict overloads for IterativeCovar to update current_estimated_rank attribute

--- iterative_covar_sgd.py
+++ iterative_covar_sgd.py
@@ -18,7 +18,7 @@ class IterativeCovarTrainer(CovarTrainer):
                 coords,eigen_forward = wiener_coords(self._orig_dataset,eigenvecs,eigenvals,return_eigen_forward=True)
                 coords = coords.to('cpu')
                 eigen_span_im = torch.sum(coords[:,:,None,None] * eigen_forward,dim=1)
-                self.train_data.dataset.images = self._orig_dataset.images - complete_eigen_span_im 
+                self.train_data.dataset.images = self._orig_dataset.images - eigen_span_im 
             else:
                 data_len = len(self.train_data.dataset)
                 rank = self.process_ind[0]
@@ -26,15 +26,13 @@ class IterativeCovarTrainer(CovarTrainer):
                 samples_per_process = data_len // world_size
                 start_ind = rank * samples_per_process
                 end_ind = start_ind + samples_per_process if rank != world_size - 1 else data_len
-                print(f'Device {self.device} , dataset index : {(start_ind,end_ind)}')
                 coords,eigen_forward = wiener_coords(self._orig_dataset,eigenvecs,eigenvals,start_ind = start_ind,end_ind = end_ind,return_eigen_forward=True)
                 coords = coords.to('cpu')
-                print(f'Device {self.device} , coords_len : {coords.shape} , ims size : {eigen_forward.shape}')
                 eigen_span_im = torch.sum(coords[:,:,None,None] * eigen_forward,dim=1)
+
                 updated_parts = [torch.zeros_like(eigen_span_im) for _ in range(world_size)]
                 dist.all_gather(updated_parts, eigen_span_im)
                 complete_eigen_span_im = torch.cat(updated_parts)
-                print(f'Device {self.device} , eigen_span_im : {eigen_span_im.shape} , updated_data : {(complete_eigen_span_im.shape,complete_eigen_span_im.device)}')
                 self.train_data.dataset.images = self._orig_dataset.images - complete_eigen_span_im
             
 
@@ -45,16 +43,16 @@ class IterativeCovar(Covar):
         
         self.current_estimated_rank = 0
     
-        self.fixed_vectors = torch.zeros((0,resolution,resolution,resolution),dtype=self.dtype)
+        self.fixed_vectors = torch.zeros((rank,resolution,resolution,resolution),dtype=self.dtype)
         self.fixed_vectors = torch.nn.Parameter(self.fixed_vectors,requires_grad = False)
-        self.fixed_vectors_ampl = torch.nn.Parameter(torch.zeros(0),requires_grad = False)
+        self.fixed_vectors_ampl = torch.nn.Parameter(torch.zeros(rank),requires_grad = False)
 
 
     @property
     def eigenvecs(self):
-        fixed_vectors = self.fixed_vectors.clone().reshape((self.current_estimated_rank,-1)) * self.fixed_vectors_ampl.reshape((self.current_estimated_rank,-1))
+        fixed_vectors = self.fixed_vectors[:self.current_estimated_rank].clone().reshape((self.current_estimated_rank,-1)) * self.fixed_vectors_ampl[:self.current_estimated_rank].reshape((self.current_estimated_rank,-1))
         _,eigenvals,eigenvecs = torch.linalg.svd(fixed_vectors,full_matrices = False)
-        eigenvecs = eigenvecs.reshape(self.fixed_vectors.shape)
+        eigenvecs = eigenvecs.reshape((self.current_estimated_rank,self.resolution,self.resolution,self.resolution))
         eigenvals = eigenvals ** 2
         return eigenvecs,eigenvals
 
@@ -63,7 +61,7 @@ class IterativeCovar(Covar):
             return
         with torch.no_grad():
             vectors = self.vectors.reshape(1,-1)
-            fixed_vectors = self.fixed_vectors.reshape((self.current_estimated_rank,-1))
+            fixed_vectors = self.fixed_vectors[:self.current_estimated_rank].reshape((self.current_estimated_rank,-1))
             coeffs = vectors @ fixed_vectors.T
             self.vectors.data.copy_((vectors - coeffs @ fixed_vectors).view_as(self.vectors))
 
@@ -72,7 +70,19 @@ class IterativeCovar(Covar):
             vector = self.vectors.detach().clone()
             vector_ampl = torch.norm(vector).reshape(1)
             normalized_vector = vector/vector_ampl
-            self.fixed_vectors = torch.nn.Parameter(torch.cat((self.fixed_vectors,normalized_vector),dim=0), requires_grad=False)
-            self.fixed_vectors_ampl = torch.nn.Parameter(torch.cat((self.fixed_vectors_ampl,vector_ampl)), requires_grad=False)
+            
+            self.fixed_vectors.data[self.current_estimated_rank] = normalized_vector
+            self.fixed_vectors_ampl.data[self.current_estimated_rank] = vector_ampl 
             self.vectors.data.copy_((torch.randn((1,self.resolution,self.resolution,self.resolution),dtype=self.dtype))/(self.resolution ** 1))
             self.current_estimated_rank += 1
+
+    def state_dict(self,*args,**kwargs):
+        state_dict = super().state_dict(*args,**kwargs)
+        state_dict.update({'current_estimated_rank' : self.current_estimated_rank})
+        return state_dict
+    
+    def load_state_dict(self,state_dict,*args,**kwargs):
+        self.current_estimated_rank = state_dict.pop('current_estimated_rank')
+        super().load_state_dict(state_dict, *args, **kwargs)
+        return
+        
