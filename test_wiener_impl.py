35968f4c1155baa2d29e8f6a9f1499f90f4c8d07
RoeyYadgar <roaiyadgar@gmail.com>
2024-08-13 12:59:31 +0300

    added latentMAP etimator in wiener_coords.py which is equivalent to wiener_coords with faster computation

--- test_wiener_impl.py
+++ test_wiener_impl.py
@@ -5,7 +5,7 @@ from covar_sgd import CovarDataset,Covar
 from aspire.operators import RadialCTFFilter
 from aspire.denoising import src_wiener_coords
 import time
-from wiener_coords import wiener_coords
+from wiener_coords import wiener_coords,latentMAP
 import torch
 import numpy as np
 
@@ -30,7 +30,8 @@ class TestWienerImpl(unittest.TestCase):
             unique_filters=[RadialCTFFilter(defocus=d) for d in np.linspace(1.5e4, 2.5e4, 7)]
             )
         mean_vol = Volume(np.mean(vols,axis=0))
-        dataset = CovarDataset(source,0,vectorsGD=None,mean_volume=mean_vol)
+        noise_var = 1
+        dataset = CovarDataset(source,noise_var,vectorsGD=None,mean_volume=mean_vol)
         device = torch.device('cuda:0')
 
 
@@ -41,15 +42,22 @@ class TestWienerImpl(unittest.TestCase):
         coords = wiener_coords(dataset,eigenvecs,eigenvals)
         elapsed_time_impl = time.time() - t
 
+        t = time.time()
+        coords2 = latentMAP(dataset,eigenvecs,eigenvals)
+        elapsed_time_implMap = time.time() - t
+
         eigenvecs = Volume(eigenvecs.cpu().numpy())
         eigenvals = np.diag(eigenvals.cpu().numpy())
         t = time.time()
-        coords_aspire = src_wiener_coords(source,mean_vol,eigenvecs,eigenvals,noise_var = 0)
+        coords_aspire = src_wiener_coords(source,mean_vol,eigenvecs,eigenvals,noise_var = noise_var)
         elapsed_time_aspire = time.time() - t
 
         np.testing.assert_allclose(coords.cpu().numpy(),coords_aspire.T,rtol = 1e-6,atol = 1e-2)
+        torch.testing.assert_close(coords,coords2,rtol=1e-6,atol=1e-2)
         print(f'Elapsed time of aspire implementation : {elapsed_time_aspire}')
         print(f'Elapsed time of own implementation : {elapsed_time_impl}')
+        print(f'Elapsed time of MAP implementation : {elapsed_time_implMap}')
+
 
 
 if __name__ == "__main__":
--- wiener_coords.py
+++ wiener_coords.py
@@ -54,3 +54,47 @@ def wiener_coords(dataset,eigenvecs,eigenvals,batch_size = 8,start_ind = None,en
 
 
     
+def latentMAP(dataset,eigenvecs,eigenvals,batch_size=8,start_ind = None,end_ind = None,return_eigen_forward = False):
+    if(start_ind is None):
+        start_ind = 0
+    if(end_ind is None):
+        end_ind = len(dataset)
+    vol_shape = eigenvecs.shape
+    L = vol_shape[-1]
+    rank = eigenvecs.shape[0]
+    dtype = eigenvecs.dtype
+    device = eigenvecs.device
+    
+    filters = dataset.unique_filters.to(device)
+    if(len(eigenvals.shape) == 1):
+        eigenvals = torch.diag(eigenvals)
+
+    nufft_plans = [NufftPlan((L,)*3,batch_size=rank,dtype = dtype,device=device) for i in range(batch_size)]
+    coords = torch.zeros((end_ind-start_ind,rank),device=device)
+    if(return_eigen_forward):
+        eigen_forward_images = torch.zeros((end_ind-start_ind,rank,L,L),dtype=dtype)
+    for i in range(0,coords.shape[0],batch_size):
+        images,pts_rot,filter_indices = dataset[start_ind + i:min(start_ind + i + batch_size,end_ind)]
+        num_ims = images.shape[0]
+        pts_rot = pts_rot.to(device)
+        images = images.to(device).reshape(num_ims,-1)
+        batch_filters = filters[filter_indices] if len(filters) > 0 else None
+        for j in range(num_ims):
+            nufft_plans[j].setpts(pts_rot[j])
+        
+        eigen_forward = vol_forward(eigenvecs,nufft_plans[:num_ims],batch_filters)
+        if(return_eigen_forward):
+            eigen_forward_images[i:i+num_ims] = eigen_forward.to('cpu')
+        eigen_forward = eigen_forward.reshape((num_ims,rank,-1))
+
+        for j in range(num_ims):
+            m = eigen_forward[j] @ eigen_forward[j].T / dataset.noise_var + torch.inverse(eigenvals)
+            im_coor = eigen_forward[j] @ images[j].T / dataset.noise_var
+            coords[i+j,:] = torch.inverse(m) @ im_coor
+
+
+
+    if(not return_eigen_forward):
+        return coords
+    else:
+        return coords,eigen_forward_images
