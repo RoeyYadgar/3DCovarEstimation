Fixed fourier reg term computation using eigenvalue instead of sqrt(eigenvalue). Added non-halfsets fourier reg scheme

covar_distributed.py
@@ -49,15 +49,23 @@ def ddp_train(rank,world_size,covar_model,dataset,batch_size_per_proc,savepath =
 
 
     if(not use_halfsets):
-        trainer.train(**kwargs)
-    else:
         num_epochs = kwargs.pop('max_epochs')
         trainer.setup_training(**kwargs)
-        trainer.fourier_reg *= 0
         for _ in range(num_reg_update_iters):
             trainer.train_epochs(num_epochs,restart_optimizer=True)
             eigenvecs = trainer.covar.eigenvecs
-            eigenvecs = eigenvecs[0] * eigenvecs[1].reshape(-1,1,1,1)
+            eigenvecs = eigenvecs[0] * (eigenvecs[1]**0.5).reshape(-1,1,1,1)
+            trainer.compute_fourier_reg_term(eigenvecs)
+            trainer.covar.orthogonal_projection()
+        trainer.train_epochs(num_epochs,restart_optimizer=True)
+        trainer.complete_training()
+    else:
+        num_epochs = kwargs.pop('max_epochs')
+        trainer.setup_training(**kwargs)
+        for _ in range(num_reg_update_iters):
+            trainer.train_epochs(num_epochs,restart_optimizer=True)
+            eigenvecs = trainer.covar.eigenvecs
+            eigenvecs = eigenvecs[0] * (eigenvecs[1]**0.5).reshape(-1,1,1,1)
             eigenvecs_list = [torch.zeros_like(eigenvecs) for _ in range(world_size)]
             dist.all_gather(tensor_list = eigenvecs_list,tensor = eigenvecs)
             #eigenvecs_list will have the same eigenvecs in each distributed group (i.e. [eigenvecs1,...,eigenvecs1,eigenvesc2,...,eigenvecs2])

covar_sgd.py
@@ -265,6 +265,8 @@ class CovarTrainer():
 
         self.filter_gain = self.dataset.get_total_gain().to(self.device)
         self.num_reduced_lr_before_stop = 4
+        self.fourier_reg = None
+        self.reg_scale = 1/(len(self.dataset)) #The sgd is performed on cost/batch_size + reg_term while its supposed to be sum(cost) + reg_term. This ensures the regularization term scales in the appropirate manner
 
     @property
     def dataset(self):
@@ -388,13 +390,6 @@ class CovarTrainer():
         else:
             self.nufft_plans = NufftPlan(vol_shape,batch_size = rank, dtype=dtype,device=self.device)
 
-        L = self.dataset.resolution
-        vgd = self.dataset.vectorsGD.reshape((-1,L,L,L)).to(self.device)
-        vectorsGD_rpsd = rpsd(*vgd)
-        vectorsGD_rpsd[:,L//2:] = vectorsGD_rpsd[:,L//2].unsqueeze(1) #TODO : PSD on volume corners are extremly low, validate if this is needed
-        self.fourier_reg = (self.noise_var) / (torch.sum(expand_fourier_shell(vectorsGD_rpsd,L,3),dim=0)) #TODO : validate this regularization term        
-        self.reg_scale = 1/(len(self.dataset)) #The sgd is performed on cost/batch_size + reg_term while its supposed to be sum(cost) + reg_term. This ensures the regularization term scales in the appropirate manner
-
         print(f'Actual learning rate {lr}')
         self.reg_scale*=reg
 
@@ -426,6 +421,10 @@ class CovarTrainer():
         if(self.optimize_in_fourier_domain):#Transform back to spatial domain            
             self.dataset.to_spatial_domain()
 
+    def compute_fourier_reg_term(self,eigenvecs):
+        eigen_rpsd = rpsd(*eigenvecs)
+        #vectorsGD_rpsd[:,L//2:] = vectorsGD_rpsd[:,L//2].unsqueeze(1) #TODO : PSD on volume corners are extremly low, validate if this is needed
+        self.fourier_reg = (self.noise_var) / expand_fourier_shell(eigen_rpsd,self.covar.resolution,3) #TODO : validate this regularization term        
 
     def log_training(self):
         if(len(self.log_epoch_ind) != 0):
@@ -447,6 +446,7 @@ class CovarTrainer():
     def results_dict(self):
         ckp = self.covar.state_dict()
         ckp['vectorsGD'] = self.vectorsGD
+        ckp['fourier_reg'] = self.fourier_reg
         ckp['log_epoch_ind'] = self.log_epoch_ind
         ckp['log_cosine_sim'] = self.log_cosine_sim
         ckp['log_fro_err'] = self.log_fro_err
@@ -467,10 +467,10 @@ def update_fourier_reg(trainer1,trainer2):
     current_fourier_reg = trainer1.fourier_reg
     #Get the covariance eigenvectors from each trainer
     eigenvecs1 = trainer1.covar.eigenvecs
-    eigenvecs1 = eigenvecs1[0] * eigenvecs1[1].reshape(-1,1,1,1)
+    eigenvecs1 = eigenvecs1[0] * (eigenvecs1[1]**0.5).reshape(-1,1,1,1)
     
     eigenvecs2 = trainer2.covar.eigenvecs
-    eigenvecs2 = eigenvecs2[0] * eigenvecs2[1].reshape(-1,1,1,1)
+    eigenvecs2 = eigenvecs2[0] * (eigenvecs2[1]**0.5).reshape(-1,1,1,1)
 
     new_fourier_reg_tensor = compute_updated_fourier_reg(eigenvecs1,eigenvecs2,filter_gain,current_fourier_reg,rank,L,trainer1.noise_var)
 
@@ -478,6 +478,9 @@ def update_fourier_reg(trainer1,trainer2):
     trainer2.fourier_reg = new_fourier_reg_tensor.to(trainer2.device)
 
 def compute_updated_fourier_reg(eigenvecs1,eigenvecs2,filter_gain,current_fourier_reg,rank,L,noise_var):
+
+    if(current_fourier_reg is None):
+        current_fourier_reg = torch.zeros((L,)*3,dtype=filter_gain.dtype,device=filter_gain.device)
 
     filter_gain_shell_correction = average_fourier_shell(filter_gain / (filter_gain + current_fourier_reg+1e-12)**2) / average_fourier_shell(filter_gain**2 / (filter_gain + current_fourier_reg+1e-12)**2)
     #filter_gain_shell_correction = 1/average_fourier_shell(filter_gain)
@@ -550,6 +553,13 @@ class Covar(torch.nn.Module):
     
     def init_random_vectors(self,num_vectors):
         return (torch.randn((num_vectors,) + (self.resolution,) * 3,dtype=self.dtype)) * (self.pixel_var_estimate ** 0.5)
+    
+    def init_random_vectors_from_psd(self,num_vectors,psd):
+        vectors = (torch.randn((num_vectors,) + (self.resolution,) * 3,dtype=self.dtype))
+        vectors_fourier = centered_fft3(vectors) / (self.resolution**1.5)
+        vectors_fourier *= torch.sqrt(psd)
+        vectors = centered_ifft3(vectors_fourier).real
+        return vectors
 
     def cost(self,images,nufft_plans,filters,noise_var,reg_scale = 0,fourier_reg = None):
         return self.cost_func(self.get_vectors(),images,nufft_plans,filters,noise_var,reg_scale,fourier_reg)
@@ -724,10 +734,6 @@ def trainCovar(covar_model,dataset,batch_size,savepath = None,**kwargs):
                                                 num_workers=num_workers,prefetch_factor=10,persistent_workers=True,pin_memory=True,pin_memory_device=str(covar_model.device))
         trainer2 = CovarTrainer(covar_model_copy,dataloader2,covar_model_copy.device,save_path=None)
         trainer2.setup_training(**kwargs)
-
-        #Start with no reg
-        trainer1.fourier_reg *= 0
-        trainer2.fourier_reg *= 0
 
         for i in range(0,num_reg_update_iters):
             trainer1.train_epochs(num_epochs,restart_optimizer=True)